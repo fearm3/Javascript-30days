11692c2229f7806d8d6cd80c9cb3e994b4f6fe59
fearm3 <ibraaa1628@gmail.com>
2022-01-17 23:30:28 +0300

    add day1

---
+++ .30DaysOfJS/day2.js
@@ -0,0 +1,5 @@
+//! Js primitive data types
+//* String,Number,Boolean,undefined,Null,Symbol
+
+//! Js non-primitive data types
+//* Object References (Object,Functions,Arrays)
---
+++ .30DaysOfJS/introduction.js
@@ -0,0 +1,25 @@
+console.log("This is coming from introduction.js");
+//! Js primitive data types
+//* String,Number,Boolean,undefined,Null,Symbol
+
+//! Js non-primitive data types
+//* Object References (Object,Functions,Arrays)
+
+//!Undefined => do not assign a value.
+
+// let firstName;
+// console.log(firstName); //Undefined!
+
+//! Null => Empty value
+
+// console.log(typeof "JS"); //!typeof
+
+//* Variable => var,let,const
+
+//! var => not used
+//! let => changes at a different time
+//! let => do not change at all
+
+//! use Camelcase name
+
+// Assaigment operator "="
